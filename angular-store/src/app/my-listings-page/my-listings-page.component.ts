8604bcd29b87fda3d0eb4818fcc9c76f60da7c8b
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2020-10-07 15:16:16 -0600

    initial push with pages

---
+++ angular-store/src/app/my-listings-page/my-listings-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeMyListings }  from '../fake-data'
+
+
+@Component({
+  selector: 'app-my-listings-page',
+  templateUrl: './my-listings-page.component.html',
+  styleUrls: ['./my-listings-page.component.css']
+})
+export class MyListingsPageComponent implements OnInit {
+  listings : Listing[] = []
+  constructor() { }
+
+  ngOnInit(): void {
+    this.listings = fakeMyListings;
+  }
+
+}
---
+++ angular-store/src/app/new-listings/new-listings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-listings',
+  templateUrl: './new-listings.component.html',
+  styleUrls: ['./new-listings.component.css']
+})
+export class NewListingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
