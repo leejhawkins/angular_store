f86fb7aeebd81a1f9deb7cadadfb9d896f12cb7d
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2020-10-19 19:46:06 -0600

    fixed environment

--- angular-store/src/environments/environment.ts
+++ angular-store/src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: "AIzaSyDiSLdxeETK57CivPiI_-u5tlOmxzVTFf4",
+    authDomain: "angular-store-front.firebaseapp.com",
+    databaseURL: "https://angular-store-front.firebaseio.com",
+    projectId: "angular-store-front",
+    storageBucket: "angular-store-front.appspot.com",
+    messagingSenderId: "480268446811",
+    appId: "1:480268446811:web:be296f024598b8f8bda37e",
+    measurementId: "G-7PHBCL2J34"
+  }
 };
 
 /*
